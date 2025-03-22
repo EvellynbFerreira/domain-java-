6cb3ef3188dbd353117b7f3bae316d3a5d6810c2
EvellynbFerreira <ferreirab.evellyn2@gmail.com>
2025-03-21 22:51:56 -0300

    Exercicio durante a aula

--- src/LeituraDados.java
+++ src/LeituraDados.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class LeituraDados {
+    public static void main(String[] args) {
+        //Criar o objeto reponsável por ler as informações
+        Scanner leitor = new Scanner(System.in);
+
+        //Criar uma variável e lê um valor do tipo float
+        System.out.println("Digite o valor do produto");
+        float valor = leitor.nextFloat();
+
+        //Lê a quantidade do produto
+        System.out.println("Qual a quantida do produto?");
+        int quantidade = leitor.nextInt();
+
+        // Exibir valor total
+        double valorFinal = valor * quantidade;
+        System.out.println(valorFinal);
+    }
 }
--- src/OperadoresAritimeticos.java
+++ src/OperadoresAritimeticos.java
@@ -1,10 +1,13 @@
 public class OperadoresAritimeticos {
     public static void main(String[] args) {
         //Declarar 3 variáveis (cp, challenge, gs)
-
+        float cp = 10;
+        float challenge = 8;
+        float gs = 7;
         //Calcular a média do semestre
-
+        double media = cp * 20/100 + challenge * 0.2 + gs *0.6;
         //Exibir a média do semestre
-        s
+        System.out.println("Media: " + media);
+
     }
 }
